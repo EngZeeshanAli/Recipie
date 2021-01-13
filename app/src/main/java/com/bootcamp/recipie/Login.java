551ab94dddea13a1b21381c4167eea3d79783310
zesha <zeshan.lhr.pk@gmail.com>
2021-01-13 21:14:21 +0500

    updated module

---
+++ app/src/main/java/com/bootcamp/recipie/Login.java
@@ -0,0 +1,76 @@
+package com.bootcamp.recipie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth myAuth;
+    private Button signIn;
+    private EditText email, password;
+    private TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init() {
+        myAuth = FirebaseAuth.getInstance();
+        signIn = findViewById(R.id.login_signin);
+        signIn.setOnClickListener(this);
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.go_to_signUn);
+        signUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_signin:
+                if (email.getText().toString().isEmpty()) {
+                    email.setError("is epmty");
+                }
+                if (password.getText().toString().isEmpty()) {
+                    password.setError("is epmty");
+                } else {
+                    String emailS = email.getText().toString();
+                    String passwordS = password.getText().toString();
+                    myAuth.signInWithEmailAndPassword(emailS, passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Login.this, "you have logined", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                break;
+            case R.id.go_to_signUn:
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/bootcamp/recipie/Register.java
@@ -0,0 +1,86 @@
+package com.bootcamp.recipie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth myAuth;
+    private Button signUp;
+    private EditText email, password;
+    private TextView signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        myAuth = FirebaseAuth.getInstance();
+        signUp = findViewById(R.id.register_signUp);
+        signUp.setOnClickListener(this);
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
+        signIn = findViewById(R.id.go_to_signin);
+        signIn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register_signUp:
+                if (email.getText().toString().isEmpty()) {
+                    email.setError("IT IS EMPTY");
+                }
+                if (password.getText().toString().isEmpty() || password.getText().toString().length() < 8) {
+                    password.setError("Some Problem");
+                } else {
+                    String emailS = email.getText().toString();
+                    String passwordS = password.getText().toString();
+                    myAuth.createUserWithEmailAndPassword(emailS, passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent login = new Intent(Register.this, Login.class);
+                                startActivity(login);
+                                Toast.makeText(Register.this, "Sign Up Successfully!", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(Register.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                break;
+            case R.id.go_to_signin:
+                Intent login = new Intent(Register.this, Login.class);
+                startActivity(login);
+                finish();
+                break;
+        }
+    }
+}
